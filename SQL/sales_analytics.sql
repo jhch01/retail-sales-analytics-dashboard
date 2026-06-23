1a4a6ddedc616d7b2e2827cfc69e68dfe49f1e36
jhch01 <161295911+jhch01@users.noreply.github.com>
2026-06-23 14:59:00 -0700

    Update sales_analytics.sql

--- SQL/sales_analytics.sql
+++ SQL/sales_analytics.sql
@@ -1,5 +1,80 @@
 -- ==================================
 -- SALES ANALYTICS
--- Purpose: Analyze revenue trends, product performance, and sales patterns
 -- ==================================
--- KPI: TOTAL REVENUE & TOTAL QUANTITY SOLD
+-- Purpose: Analyze revenue trends, product performance, and sales patterns
+
+-- ==================================
+-- KPI: Total revenue & quantity sold
+-- ==================================
+SELECT 
+  SUM(`Total Amount`) AS total_revenue, 
+  SUM(`Quantity`) AS total_quantity_sold
+FROM `project-5fb8896c-d461-49e8-a4c.retail_project.orders`;
+
+-- ==================================
+-- Revenue by month
+-- ==================================
+SELECT 
+  d.month_name, 
+  SUM(o.`Total amount`) AS revenue
+FROM `project-5fb8896c-d461-49e8-a4c.retail_project.orders` AS o
+INNER JOIN `project-5fb8896c-d461-49e8-a4c.retail_project.dates` AS d
+  ON o.Date = d.Date
+GROUP BY d.month_name
+ORDER BY revenue DESC;
+-- Revenue by weekday
+SELECT 
+  d.weekday, 
+  SUM(`Total Amount`) AS total_revenue
+FROM `project-5fb8896c-d461-49e8-a4c.retail_project.sales_fact` AS sf
+INNER JOIN `project-5fb8896c-d461-49e8-a4c.retail_project.dates` AS d
+  ON sf.Date = d.Date
+GROUP BY d.weekday
+ORDER BY total_revenue DESC;
+-- Product / Sales behavior: Revenue by Product Category
+SELECT 
+  `Product Category`, 
+  SUM(`Total Amount`) AS revenue
+FROM `project-5fb8896c-d461-49e8-a4c.retail_project.sales_fact`
+GROUP BY `Product Category`;
+-- Customer segment analysis: Top customers + revenue contribution
+WITH customer_revenue AS (
+  SELECT `Customer ID`, SUM(`Total Amount`) AS total_spent
+  FROM `project-5fb8896c-d461-49e8-a4c.retail_project.sales_fact` 
+  GROUP BY `Customer ID` 
+), 
+
+total_revenue AS (
+  SELECT SUM(total_spent) AS overall_revenue
+  FROM customer_revenue
+)
+
+SELECT 
+  cr.`Customer ID`, 
+  cr.total_spent, 
+  ROUND(cr.total_spent/tr.overall_revenue * 100, 2) AS revenue_share
+FROM customer_revenue AS cr
+CROSS JOIN total_revenue AS tr
+ORDER BY cr.total_spent DESC;
+-- Revenue by age group 
+SELECT CASE
+  WHEN cd.age BETWEEN 18 AND 25 THEN '18-25'
+  WHEN cd.age BETWEEN 26 AND 35 THEN '26-35'
+  WHEN cd.age BETWEEN 36 AND 50 THEN '36-50'
+  ELSE '50+'
+  END AS age_group, 
+  SUM(sf.`Total Amount`) AS total_revenue
+FROM `project-5fb8896c-d461-49e8-a4c.retail_project.sales_fact` AS sf
+INNER JOIN `project-5fb8896c-d461-49e8-a4c.retail_project.customers_dim` AS cd
+  ON sf.`Customer ID` = cd.`Customer ID`
+GROUP BY age_group 
+ORDER BY age_group;
+-- Revenue by gender
+SELECT 
+  cd.gender, 
+  SUM(`Total Amount`) AS total_revenue
+FROM `project-5fb8896c-d461-49e8-a4c.retail_project.sales_fact` AS sf
+INNER JOIN `project-5fb8896c-d461-49e8-a4c.retail_project.customers_dim` AS cd
+  ON sf.`Customer ID` = cd.`Customer ID`
+GROUP BY cd.gender
+ORDER BY total_revenue DESC;
